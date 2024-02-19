b3d3d3016855f1545481fc087f8ae2541a2cb936
DevinJG <86039118+DevinJG@users.noreply.github.com>
2024-02-18 17:18:40 -0700

    Update c.js

--- c.js
+++ c.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: " https://cnt-b57312df-5d1b-4bef-a0ba-5656902316be.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-5c812ae4-0ea0-497b-9e7e-e01fe7b7a4da.containerhub.tripleten-services.com"
 }
